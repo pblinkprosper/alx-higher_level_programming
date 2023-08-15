89eab75a7e82deb8788dca237b2748b2ff5de2e8
Prosper <pblinkprosper01@gmail.com>
2023-08-15 21:54:49 +0000

    CPython

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+        size_t size;
+        size_t i;
+        PyObject *obj;
+
+        size = PyList_Size(p);
+        printf("[*] Size of the Python List = %zu\n", size);
+        printf("[*] Allocated = %zu\n", ((PyListObject *)p)->allocated);
+
+        for (i = 0; i < size; i++)
+        {
+                obj = PyList_GET_ITEM(p, i);
+                printf("Element %zu: %s\n", i, Py_TYPE(obj)->tp_name);
+        }
+}
