3597e06505405999b8f32129f60e45ee25d22ed6
Prosper <pblinkprosper01@gmail.com>
2023-08-28 20:43:14 +0000

    Check for only intergers indent correction

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             index += 1
         except (ValueError, TypeError):
             pass
-        print()
-        return index
+    print()
+    return index
