8013f62592f94731cefab283269eb87a11deffca
Prosper <pblinkprosper01@gmail.com>
2023-08-28 16:46:51 +0000

    Added else statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,9 @@ def safe_print_list(my_list=[], x=0):
     for elements in range(x):
         try:
             print("{}".format(my_list[elements]), end="")
-            i += 1
         except IndexError:
             break
+        else:
+            i += 1
         print()
         return i
