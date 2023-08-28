8d43066d981f68e425df23b326939ed2ddd0950d
Prosper <pblinkprosper01@gmail.com>
2023-08-28 21:55:23 +0000

    Magic calculation3

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             else:
                 result += a**b / i
-        except NameError:
+        except TypeError:
             result = b + a
             break
     return result
