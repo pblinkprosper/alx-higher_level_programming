700959c7a79dc7f965f1f20fb37814c92e75ca71
Prosper <pblinkprosper01@gmail.com>
2023-08-30 22:16:38 +0000

    Corrected spelling errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
 
     def __init__(self, size=0):
         if (type(size) is not int):
-            raise TypeError("size must be a integer")
+
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
