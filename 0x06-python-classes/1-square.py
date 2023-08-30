33cc56a8d2d78749fa77f385ada89ddc7db6f722
Prosper <pblinkprosper01@gmail.com>
2023-08-30 17:02:26 +0000

    Task square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is a square class based on the 0-square.py"""
+
+
+class Square:
+    """This square class defines a square'"""
+    def __init__(seld, size=None):
+        """Initialising private attribute 'size'"""
+        self.__size = size
