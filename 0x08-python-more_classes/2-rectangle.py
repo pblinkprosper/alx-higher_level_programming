a2550588d00a6f73f20aa44407809b7adc9840f9
Prosper <pblinkprosper01@gmail.com>
2023-09-04 23:11:46 +0000

    Rectangle class with height, width and perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Rectangle class defines a rectangle based on 1-rectangle"""
+
+
+class Rectangle:
+    """A Rectangle class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height
+
+    @property
+    def width(self)
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >=")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width + self.height) * 2
