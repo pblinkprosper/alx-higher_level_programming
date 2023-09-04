cbed3cb68cab2ceb776b112453ea5c3183c0f4eb
Prosper <pblinkprosper01@gmail.com>
2023-09-04 23:40:48 +0000

    Rectangle deletion

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Rectangle class that defines rectangle by 4-rectangle.py"""
+
+
+class Rectangle:
+    """A rectangle class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+        string = ""
+        for i in range(self.height):
+            string += '#' * self.width + '\n'
+        return string[:-1]
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        print('Bye rectangle...')
