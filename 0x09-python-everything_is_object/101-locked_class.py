857464da9b6de9fb5927df59e811f6d0d4805336
Prosper <pblinkprosper01@gmail.com>
2023-09-06 22:28:08 +0000

    task 30 redone

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""LockedClass that prevents dynamic attributes creation"""
-
-
 class LockedClass:
 
-    __slots__ = ["first_name"]
+    __slots__ = "first_name"
 
-    def __init__(self):
-        pass
+    def __init__(self, first_name=None):
+        self. first_name = first_name
