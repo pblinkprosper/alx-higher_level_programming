5e2ef7fb32718c4e87fd5607b62adde5f92a4d0d
Prosper <pblinkprosper01@gmail.com>
2023-09-08 10:57:59 +0000

    Task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Matrix division"""
+
+
+def matrix_divided(matrix, div):
+    """Divide a matrix"""
+
+    listError = 'matrix must be a matrix (list of lists) of integers/floats'
+    sizeError = 'Each row of the matrix must have the same size'
+    if type(matrix) is not list:
+        raise TypeError(listError)
+    for item in range(len(matrix)):
+        if item is not 0:
+            result = item - 1
+            if len(matrix[item]) is not len(matrix[result]):
+                raise TypeError(sizeError)
+    if isinstance(div, int) is False:
+        raise TypeError('div must be a number')
+    if div is 0:
+        raise ZeroDivisionError('division by zero')
+
+    return [[round(item / div, 2) for item in m_list] for m_list in matrix]
