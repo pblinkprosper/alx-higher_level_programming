59d42f4c025a9e78faba3d291ff65b2fc097a601
Prosper <pblinkprosper01@gmail.com>
2023-09-13 21:50:07 +0000

    Code rewrite

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,4 +3,8 @@
 
 
 def is_same_class(obj, a_class):
-    return type(obj) == a_class
+    """Check is object is exactly instance of class"""
+
+    if type(obj) == a_class:
+        return True
+    return False
