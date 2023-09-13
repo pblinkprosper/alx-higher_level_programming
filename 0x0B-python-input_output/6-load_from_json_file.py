640e13c0e4fd05cc2e4b134d4656a51c8ec5130c
Prosper <pblinkprosper01@gmail.com>
2023-09-13 06:32:16 +0000

    create object from JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Function that creates an object from a JSON file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Create object from JSON file"""
+
+    with open(filename, "r") as file:
+        return json.load(file)
