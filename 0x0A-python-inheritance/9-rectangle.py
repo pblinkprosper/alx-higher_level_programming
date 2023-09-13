173f4abd024fcbd5a684a277d5eae8d0382107d8
Prosper <pblinkprosper01@gmail.com>
2023-09-13 23:16:29 +0000

    Rectangle correction

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -5,15 +5,18 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Defines rectangle using BaseGeometry"""
 
     def __init__(self, width, height):
-        super().integer_validator('width', width)
+        super().integer_validator("width", width)
         self.__width = width
-        super().integer_validator('height', height)
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
         return self.__width * self.__height
 
     def __str__(self):
-        return '[Rectangle] {}/{}'.format(self.__width, self.__height)
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
