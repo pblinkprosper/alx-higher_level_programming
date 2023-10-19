d93d429a8300260a70dfc50e4d19f9037b6b234e
Prosper <pblinkprosper01@gmail.com>
2023-10-19 22:56:10 +0000

    advanced tasks

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- Lists all shows not linked to Dexter
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN
+(SELECT tv_genres.name
+from tv_genres
+LEFT JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter')
+ORDER BY tv_genres.name ASC
