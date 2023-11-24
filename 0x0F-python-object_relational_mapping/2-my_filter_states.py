617ea633c14e85ba1aacd16e922e87bdb89bc03c
Prosper <pblinkprosper01@gmail.com>
2023-11-24 15:59:21 +0000

    user argument match

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Takes in argument and displays all values in states table
+    where name matches argument.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}'
+                ORDER BY states.id ASC".format(sys.argv[4]).strip("'"))
+    [print(state) for state in cur.fetchall()]
