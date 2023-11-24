7d32e90f77cc906dc18ce683e928494e87eca250
Prosper <pblinkprosper01@gmail.com>
2023-11-24 14:58:13 +0000

    List states in DB

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Lists all states from Database"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM 'states'")
+    [print(state) for state in cur.fetchall()]
