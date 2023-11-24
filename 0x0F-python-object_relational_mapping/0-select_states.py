e1c075537a5d61b4b8eed65c16662cd0263d1a19
Prosper <pblinkprosper01@gmail.com>
2023-11-24 14:59:43 +0000

    corrected symbols

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,5 +7,5 @@ import MySQLdb
 if __name__ == "__main__":
     conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM 'states'")
+    cur.execute("SELECT * FROM `states`")
     [print(state) for state in cur.fetchall()]
