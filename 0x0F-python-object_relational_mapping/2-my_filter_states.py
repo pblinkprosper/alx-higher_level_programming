7a423a2dbfc9542e9b5970bf7e0d9fe69a69cf9e
Prosper <pblinkprosper01@gmail.com>
2023-11-24 16:02:58 +0000

    checked indentation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,6 @@ import MySQLdb
 if __name__ == "__main__":
     con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = con.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}'
-                ORDER BY states.id ASC".format(sys.argv[4]).strip("'"))
+    cur.execute("SELECT * FROM states WHERE\
+            name '{}' ORDER BY states.id ASC".format(sys.argv[4]).strip("'"))
     [print(state) for state in cur.fetchall()]
