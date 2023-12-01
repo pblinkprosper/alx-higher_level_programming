cccd7ffc565fbf536912dd3a089fe70d7363a195
Prosper <pblinkprosper01@gmail.com>
2023-12-01 23:18:34 +0000

    fetches request

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request
+    to the URL and displays the value of the X-Request-Id variable
+"""
+import sys
+import urllib.request
+
+if __name__ == '__main__':
+    with request.urlopen(sys.argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
