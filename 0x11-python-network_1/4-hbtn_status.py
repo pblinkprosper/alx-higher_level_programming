0f68c4b8b53ad26eccc7dfe752dc4efc4637812b
Prosper <pblinkprosper01@gmail.com>
2023-12-02 00:55:30 +0000

    fetch another URL

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+
+if __name__ == "__main__":
+    url = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(url.text)))
+    print('\t- content: {}'.format(url.text))
