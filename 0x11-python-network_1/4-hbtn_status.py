e2c8488558a27239a093f04bc9ce8a2af1579942
Prosper <pblinkprosper01@gmail.com>
2023-12-02 01:02:57 +0000

    task 5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 import requests
 
 if __name__ == "__main__":
-    url = requests.get('https://intranet.hbtn.io/status')
+    url = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type: {}'.format(type(url.text)))
     print('\t- content: {}'.format(url.text))
